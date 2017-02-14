ba0f01f1ccbc1a25fbcf8ccd10940c9d432d0f90
FernandoRobles\Fernando Robles <fernando.robles@sngular.team>
2017-02-14 16:42:24 -0600

    Transacciones: parte2 ejemplos

--- Tutorial Spring/springjdbc01/src/test/java/TestPersonaServiceImpl.java
+++ Tutorial Spring/springjdbc01/src/test/java/TestPersonaServiceImpl.java
@@ -57,5 +57,49 @@ public class TestPersonaServiceImpl {
         return contadorPersonas;
     }
 
+    @Test
+    @Transactional
+    public void testOperaciones() {
+        try {
+            System.out.println();
+            logger.info("Inicio del test testOperaciones");
+            Persona persona1 = new Persona();
+            persona1.setNombre("Andrea");
+            persona1.setApePaterno("Lara");
+            persona1.setEmail("andrea.lara@ymail.com");
+            personaService.agregarPersona(persona1);
 
+            //Deberia haber 4 personas
+            assertEquals(4, personaDao.contadorPersonas());
+
+            //Actualizamos la persona con id=1
+            Persona persona2 = personaService.recuperarPersona(new Persona(1));
+            persona2.setNombre("Administrador");
+            personaService.modificarPersona(persona2);
+
+            this.desplegarPersonas();
+            System.out.println();
+
+        } catch (Exception e) {
+            logger.error("Error en Servicio: " + e);
+        }
+    }
+
+    @Test
+    public void compruebaOperaciones (){
+        try{
+            System.out.println();
+            logger.info("Fin del test compruebaOperaciones");
+
+            //Deberia haber 3 personas
+            //El caso anterior aplico rollback por:
+            //el uso de AbstractTransactionalJUnit4SpringConextTests
+            assertEquals(3, personaDao.contadorPersonas());
+            this.desplegarPersonas();
+            logger.info("Fin del test testCompruebaOperaciones");
+            System.out.println();
+        }catch(Exception e){
+            System.out.println("Error en Service: " + e);
+        }
+    }
 }
