926b85f5878355e115e173f77b637f05706ff50c
FernandoRobles\Fernando Robles <fernando.robles@sngular.team>
2017-02-16 17:41:12 -0600

    SpringNinja: Manejo de logs

--- backendninja/src/main/java/com/jfrogz/controller/Example3Controller.java
+++ backendninja/src/main/java/com/jfrogz/controller/Example3Controller.java
@@ -2,6 +2,8 @@ package com.jfrogz.controller;
 
 import com.jfrogz.model.Person;
 import com.sun.javafx.sg.prism.NGShape;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,8 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 @RequestMapping("/example3")
 public class Example3Controller {
+    private static final Log logger = LogFactory.getLog(Example3Controller.class);
+
     public static final String FORM_VIEW = "form";
     public static final String RESULT_VIEW = "result";
 
@@ -31,13 +35,17 @@ public class Example3Controller {
 
     @GetMapping("/showform")
     public String showForm(Model model) {
+        logger.info("INFO TRACE");
+        logger.warn("WARNING TRACE");
+        logger.error("ERROR TRACE");
+        logger.debug("DEBUG TRACE");
         model.addAttribute("person", new Person());
-        int error = 6/0;
         return FORM_VIEW;
     }
 
     @PostMapping("/addperson")
     public ModelAndView addPerson(@ModelAttribute("person") Person person) {
+        logger.info("METHOD: 'addPerson' -- PARAMS: '" + person);
         ModelAndView andView = new ModelAndView(RESULT_VIEW);
         andView.addObject("person", person);
         return andView;
--- backendninja/src/main/java/com/jfrogz/model/Person.java
+++ backendninja/src/main/java/com/jfrogz/model/Person.java
@@ -10,6 +10,14 @@ public class Person {
     public Person (){
     }
 
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
