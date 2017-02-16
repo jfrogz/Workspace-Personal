5f06b02f55f91e99631cd6d2e01e3093b012e8dc
FernandoRobles\Fernando Robles <fernando.robles@sngular.team>
2017-02-16 17:28:21 -0600

    SpringNinja: Uso y manejo de paginas de error para controlar el flujo

--- backendninja/src/main/java/com/jfrogz/controller/Example3Controller.java
+++ backendninja/src/main/java/com/jfrogz/controller/Example3Controller.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/example3")
@@ -16,9 +17,22 @@ public class Example3Controller {
     public static final String FORM_VIEW = "form";
     public static final String RESULT_VIEW = "result";
 
+    //forma1
+
+    @GetMapping("/")
+    public String redirect() {
+        return "redirect:/example3/showform";
+    }
+
+    @GetMapping("")
+    public RedirectView redirectView() {
+        return new RedirectView("/example3/showform");
+    }
+
     @GetMapping("/showform")
     public String showForm(Model model) {
         model.addAttribute("person", new Person());
+        int error = 6/0;
         return FORM_VIEW;
     }
 
