5101f678b5548e3f36ce0391651a2364ac68cf12
FernandoRobles\Fernando Robles <fernando.robles@sngular.team>
2017-02-16 15:29:31 -0600

    SpringNinja: Como regresar una plantilla usando @GetMapping

---
+++ backendninja/src/main/java/com/jfrogz/controller/ExampleController.java
@@ -0,0 +1,27 @@
+package com.jfrogz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/example")
+public class ExampleController {
+
+    public static final String EXAMPLE_VIEW = "example";
+    //primera forma
+    //@RequestMapping(value = "/exampleString", method = RequestMethod.GET)
+    @GetMapping("/exampleString")
+    public String exampleString() {
+        return EXAMPLE_VIEW;
+    }
+
+    //Segunda forma
+    @GetMapping("/exampleMAV")
+    public ModelAndView exampleMAV(){
+        return new ModelAndView(EXAMPLE_VIEW);
+    }
+}
