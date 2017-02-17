5207f9ac7b1fbdec5b2ef8a3a07f044708554b82
Fernando Robles <sr.frog@hotmail.com>
2017-02-16 23:45:20 -0600

    Spring MVC: Proyecto spring MVC básico

---
+++ prueba_integracion/src/main/java/com/jfrogz/util/AppConfig.java
@@ -0,0 +1,29 @@
+package com.jfrogz.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by Fernando on 16/02/2017.
+ */
+@Configuration
+@EnableWebMvc
+public class AppConfig extends WebMvcConfigurerAdapter{
+
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/");
+        resolver.setSuffix(".jsp");
+        return  resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
