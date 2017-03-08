ee49b8e2d3247beee27ab980323d5f5a8e38f76b
FernandoRobles\Fernando Robles <fernando.robles@sngular.team>
2017-03-08 16:52:24 -0600

    Se anexa un codigo ejemplo que usa Spring MVC, thymeleaf, html, para futuros ejercicios

---
+++ Notariaxxx/notarial/src/main/java/com/jfrogz/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.jfrogz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", "Fernando Robles");
+        return "hello";
+    }
+}
---
+++ Notariaxxx/notarial/src/main/webapp/index2.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Fernando Robles
+  Date: 08/03/2017
+  Time: 12:53 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Home 2</title>
+  </head>
+  <body>
+  Esto es el home 2
+  </body>
+</html>
