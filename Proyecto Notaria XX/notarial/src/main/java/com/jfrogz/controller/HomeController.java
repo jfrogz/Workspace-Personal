10a6742338dfc46ea38b0919abb7d6a7bf8acc4f
Fernando Robles <sr.frog@hotmail.com>
2017-03-08 21:40:39 -0600

    Se agrega funcionamiento del uso de  @GetMapping y el uso de variables en el path

--- Proyecto Notaria XX/notarial/src/main/java/com/jfrogz/controller/HomeController.java
+++ Proyecto Notaria XX/notarial/src/main/java/com/jfrogz/controller/HomeController.java
@@ -3,9 +3,8 @@ package com.jfrogz.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -14,5 +13,12 @@ public class HomeController {
     public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", "Fernando Robles");
         return "hello";
+    }
+
+    @GetMapping("home2/{nombre}")
+    public ModelAndView home2(@PathVariable("nombre")String nombre){
+        ModelAndView andView = new ModelAndView("hello2");
+        andView.addObject("title", nombre);
+        return andView;
     }
 }
