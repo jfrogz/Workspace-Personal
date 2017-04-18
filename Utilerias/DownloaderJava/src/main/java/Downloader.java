080abd564a827448ff747e5dec414e2ab999c149
jfrogz <sr.frog@hotmail.com>
2017-04-17 22:57:49 -0500

    Completado el programa que descarga un recurso seleccionado

--- Utilerias/DownloaderJava/src/main/java/Downloader.java
+++ Utilerias/DownloaderJava/src/main/java/Downloader.java
@@ -1,8 +1,58 @@
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
- * Created by Praxis on 17/04/2017.
+ * Created by Fernando Robles on 17/04/2017.
  */
 public class Downloader {
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+//        String url = "http://i.imgur.com/HZ1hq.jpg";
+//        String name = "lineaDeCodigo.jpg";
+        String url = "https://wallpapersite.com/images/pages/pic_w/6227.jpg";
+        String name = "Widowmaker.jpg";
+        String folder = "descargas/";
+
+        File dir = new File(folder);
+        if(!dir.exists())
+        {
+            if(!dir.mkdir())
+                return;
+        }
+
+        try {
+            File file = new File(folder + name);
+            URLConnection conn = new URL(url).openConnection();
+            conn.connect();
+
+            System.out.println("\nempezando descarga: \n");
+            System.out.println(">> URL: " + url);
+            System.out.println(">> Nombre: " + name);
+            System.out.println(">> Tamaño: " + conn.getContentLength() + " bytes");
+
+            InputStream in = conn.getInputStream();
+            OutputStream out = new FileOutputStream(file);
+
+            int b =0;
+            while (b!= -1){
+                b = in.read();
+                if(b!= -1){
+                    out.write(b);
+                }
+            }
+
+            out.close();
+            in.close();
+        } catch (MalformedURLException e) {
+            System.out.println("la url: " + url + " no es valida!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception exp){
+            System.out.println(exp);
+        }
+        System.exit(0);
     }
 }
