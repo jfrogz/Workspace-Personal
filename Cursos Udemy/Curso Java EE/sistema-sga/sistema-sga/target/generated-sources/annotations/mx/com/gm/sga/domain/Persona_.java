bbcfba37c79af848b923c3cc68fdaed25d1e8301
En Praxis <dragonslyfer@gmail.com>
2017-04-20 18:09:19 -0500

    Actualizacion del proyecto dde sistema-sga

---
+++ Cursos Udemy/Curso Java EE/sistema-sga/sistema-sga/target/generated-sources/annotations/mx/com/gm/sga/domain/Persona_.java
@@ -0,0 +1,21 @@
+package mx.com.gm.sga.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import mx.com.gm.sga.domain.Usuario;
+
+@Generated(value="EclipseLink-2.6.4.v20170112-rNA", date="2017-04-20T17:00:22")
+@StaticMetamodel(Persona.class)
+public class Persona_ { 
+
+    public static volatile SingularAttribute<Persona, String> apeMaterno;
+    public static volatile SingularAttribute<Persona, String> telefono;
+    public static volatile SetAttribute<Persona, Usuario> usuarios;
+    public static volatile SingularAttribute<Persona, String> apePaterno;
+    public static volatile SingularAttribute<Persona, String> nombre;
+    public static volatile SingularAttribute<Persona, Integer> idPersona;
+    public static volatile SingularAttribute<Persona, String> email;
+
+}
